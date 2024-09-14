09ff1890df7f13ebb72b5c3a8ac9c436505b750b
michaldrozd <michaldrozd03@gmail.com>
2024-09-14 19:26:54 +0200

    Added NullValidator

---
+++ src/Shared/EduSchedu.Shared.Abstractions/Kernel/CommandValidators/NullValidator.cs
@@ -0,0 +1,12 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace EduSchedu.Shared.Abstractions.Kernel.CommandValidators;
+
+public static class NullValidator
+{
+    public static void ValidateNotNull<T>([NotNull] T value)
+    {
+        if (value is null)
+            throw new ArgumentNullException($"{typeof(T).Name} is null");
+    }
+}
