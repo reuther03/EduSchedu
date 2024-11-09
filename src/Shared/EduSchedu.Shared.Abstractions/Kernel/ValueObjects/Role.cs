f6a3cf8c3e179ea9a2ec96f22d93f6180bdcc0f7
michaldrozd <michaldrozd03@gmail.com>
2024-11-09 03:15:04 +0100

    Added new user role

--- src/Shared/EduSchedu.Shared.Abstractions/Kernel/ValueObjects/Role.cs
+++ src/Shared/EduSchedu.Shared.Abstractions/Kernel/ValueObjects/Role.cs
@@ -5,6 +5,8 @@ public enum Role
     BackOffice = 1,
     HeadMaster = 2,
     Teacher = 3,
-    Student = 4
+    Student = 4,
+    Freelancer = 5
+    //plan: dodac endpint ktory bedzie tylko resjtorwal freelancera i bedzie mial dostep do wszystkich szkol ale ograniczony dostep do danych
 
 }
